7f49c0063558baeda40c66631c638b00762a6ddb
Sayaniku <sayaniku@proton.me>
2024-05-11 11:18:55 +0200

    Edited the index.js

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
+// Configurations of needed packages
 require('dotenv').config();
-const { Client, Collection, GatewayIntentBits, Partials, ActivityType } = require("discord.js");
+const { Client, Collection, GatewayIntentBits, Partials, ActivityType, Events } = require("discord.js");
 
+// Always use client with intents
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildModeration, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.GuildScheduledEvents, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
     partials: [Partials.Channel, Partials.GuildMember, Partials.GuildScheduledEvent, Partials.Message, Partials.Reaction, Partials.ThreadMember, Partials.User],
 })
 
+// Login with the discord token configured on the .env file
 client.login(process.env.TOKEN)
 
+// Say when the bot is ready on the terminal
+client.once(Events.ClientReady, readyClient => {
+    console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// When an word is say at the start of the phase, the bot replies
+client.on('messageCreate', (message) => {
+    if (message.author.bot) {
+        return;
+    }
+
+    if (message.content === "senko") {
+        message.reply("Je suis un bot multifonction cree par mon maitre Sayaniku, j'opere generalement dans l'antre des renards et kitsune, cependant vous pouvez me trouver sur 2/3 serveurs en plus car mon maitre a decide de m'ajouter sur ces serveurs, j'ai ete dev en meme temps que ce dernier joue a Honkai Star Rail donc il se peut que de temps a autre mon code ait des problemes");
+    }
+    if (message.content === "Senko") {
+        message.reply("Je suis un bot multifonction cree par mon maitre Sayaniku, j'opere generalement dans l'antre des renards et kitsune, cependant vous pouvez me trouver sur 2/3 serveurs en plus car mon maitre a decide de m'ajouter sur ces serveurs, j'ai ete dev en meme temps que ce dernier joue a Honkai Star Rail donc il se peut que de temps a autre mon code ait des problemes");
+    }
+});
+// Anti-crash of the bot
 async function errorHandler(error) {
     // erreurs ignorées
     if (error.code == 10062) return; // Unknown interaction
