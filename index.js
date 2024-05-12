a80d6c463a250d9a5caf0e257c275438d08b05b2
Sayaniku <sayaniku@proton.me>
2024-05-12 11:29:57 +0200

    .

--- index.js
+++ index.js
@@ -26,16 +26,8 @@ const client = new Client({
 // Config file needed to use an prefix and add the owner
 client.config = require("./config.json");
 
-// Event loading
-const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
-for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(client, ...args));
-    } else {
-        client.on(event.name, (...args) => event.execute(client, ...args));
-    }
-};
+// Handlers loading
+
 
 // Login with the discord token configured on the .env file
 client.login(process.env.TOKEN)
